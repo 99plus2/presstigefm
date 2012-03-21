1e8be61d03337c2a15ab71cba449909cdf246187
Florian Mladitsch <florian.mladitsch@googlemail.com>
2012-03-21 13:12:43 +0100

    added simple traversal

--- python/apps/pageanalyzer/main.py
+++ python/apps/pageanalyzer/main.py
@@ -1,12 +1,44 @@
 import sys
 import configparser
-import urllib
+from urllib.request import urlopen
+from urllib.parse import urlparse
+import time
+from bs4 import BeautifulSoup
 
 USAGE = """USAGE:
 $ python main.py -url page-url [-delay delay-time|]
     -url: the start url
-    -delay: the delay between requests in milliseconds
+    -delay: the delay between requests in seconds
 """
+
+def scan_page(url, delay):
+    queue = [url]
+    visited = set()
+    links = {}
+    base_url = urlparse(url).hostname
+    while queue:
+        current_url = queue[0]
+        queue = queue[1:]
+        visited.add(current_url)
+        if current_url not in links:
+            links[current_url] = []
+        html = urlopen(current_url).read()
+        soup = BeautifulSoup(html)
+        for link in soup.find_all('a'):
+            link = link.get('href')
+            if link and link.startswith('/'):
+                link = 'http://' + base_url + link
+            links[current_url].append(link)
+            if urlparse(link).hostname == base_url and link not in visited:
+                queue.append(link)
+    print(links)
+    return links
+
+def parse_to_graph(links):
+    pass
+
+def generate_file(graph, filename):
+    pass
 
 def parse_arguments(argv):
     url = None
@@ -19,6 +51,8 @@ def parse_arguments(argv):
                 delay = int(argv[argv.index(arg) + 1])
         if not url:
             raise Exception
+        if not delay:
+            delay = 0
     except:
         print(USAGE)
         sys.exit(0)
@@ -32,4 +66,6 @@ if __name__ == '__main__':
     except:
         print(USAGE)
     url, delay = parse_arguments(sys.argv)
-    print(url, delay)
+    links = scan_page(url, delay)
+    graph = parse_to_graph(links)
+    generate_file(graph, 'graph.png')
