a5a67eaf70f75c59fdd18fdf6304aa612352c59e
Florian Mladitsch <florian.mladitsch@googlemail.com>
2012-07-19 20:45:39 +0200

    method txt_to_graph now correctly returns the parsed graph

--- python/algorithms/graphreader.py
+++ python/algorithms/graphreader.py
@@ -6,6 +6,12 @@ sys.path.append('../datastructures/graph')
 from graph import Graph
 
 def txt_to_graph(filepath):
+    """ Expects a path to a file describing a graph with format:
+        [number_vertices] [number_edges]
+        v [vertex_id]
+        ...
+        e [start_vertex_id] [target_vertex_id] 
+        ... """
     graph = Graph()
     with open(filepath) as f:
         for line in f:
@@ -14,6 +20,7 @@ def txt_to_graph(filepath):
                 graph.add_vertex(tokens[1])
             elif tokens[0] == 'e':
                 graph.add_edge(tokens[1], tokens[2])
+    return graph
 
 if __name__ == '__main__':
     print('hello')
