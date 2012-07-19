d7ef7c7fabe4fe5f8ec0e00b32472f3725626120
Florian Mladitsch <florian.mladitsch@googlemail.com>
2012-07-19 22:07:10 +0200

    implemented topsort

--- python/algorithms/topsort.py
+++ python/algorithms/topsort.py
@@ -6,8 +6,30 @@ import sys
 from graphreader import txt_to_graph
 
 def topsort(graph):
-    """ Returns the topological order of the graph as a list. """
-    pass
+    """ Returns the topological order of the graph as a list. 
+        graph = [datastructures.graph.Graph]
+        returns [list(datastructures.graph.Vertex)], topological order of the graph """
+    sorted_elements = []
+    vertices_without_inedges = get_vertices_without_inedges(graph)
+    while vertices_without_inedges:
+        vertex = vertices_without_inedges.pop()
+        sorted_elements.append(vertex)
+        for successor in vertex.get_successors():
+            graph.remove_edge_between(vertex, successor)
+            if not successor.get_predecessors():
+                vertices_without_inedges.append(successor)
+    if graph.edges:
+        print('Cycle detected in graph')
+    return sorted_elements
+
+def get_vertices_without_inedges(graph):
+    """ Searches for vertices without a incoming edge.
+        returns [list(datastructures.graph.Vertex)] """
+    candidates = graph.vertices.copy()
+    for edge in graph.edges:
+        if edge.target_vertex.name in candidates:
+            candidates.pop(edge.target_vertex.name)
+    return [candidates[key] for key in candidates.keys()]
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
